8de610a2141649bfd5b05fc79249060f90baa4c4
Ying Her <yher.prime@gmail.com>
2024-03-24 23:44:31 -0500

    useHistory doesn't work for the ThankYouPage so I'm revert back to using Material UI Link

--- src/components/ThankYouPage/ThankYouPage.jsx
+++ src/components/ThankYouPage/ThankYouPage.jsx
@@ -1,35 +1,34 @@
 import { Button, Link } from '@mui/material';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+// import { useHistory } from 'react-router-dom';
 
 function ThankYouPage() {
+  // const history = useHistory();
+
   const handleClick = () => {
-    console.log('in handleClick');
+    console.log('in ThankYou handleClick');
   };
 
   return (
     <div className="thankyou-messages">
       <p>Your order has been placed! Thank you for your business.</p>
-      <Link to="/home">
-        <Button
-          onClick={handleClick}
-          size="small"
-          variant="outlined"
-          startIcon={<ArrowBackIosIcon />}
-        >
-          Back to Home
-        </Button>
-      </Link>{' '}
-      <Link to="/products">
-        <Button
-          onClick={handleClick}
-          size="small"
-          variant="outlined"
-          endIcon={<ArrowForwardIosIcon />}
-        >
-          Continue Shopping
-        </Button>
-      </Link>
+      <Button
+        onClick={handleClick}
+        size="small"
+        variant="outlined"
+        startIcon={<ArrowBackIosIcon />}
+      >
+        <Link to="/home">Back to Home</Link>
+      </Button>{' '}
+      <Button
+        onClick={handleClick}
+        size="small"
+        variant="outlined"
+        endIcon={<ArrowForwardIosIcon />}
+      >
+        <Link to="/products">Continue Shopping</Link>
+      </Button>
     </div>
   );
 }
