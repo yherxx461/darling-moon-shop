6b3cec2eec698c46c60c9dc490827f8fd25ffd8a
Ying Her <yher.prime@gmail.com>
2024-03-24 23:46:10 -0500

    Framework created for Products Page

--- src/components/ProductsPage/ProductsPage.jsx
+++ src/components/ProductsPage/ProductsPage.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 
 function ProductsPage() {
+  const history = useHistory();
+
   const handleClick = () => {
     history.push('/products');
     console.log('in handleClick navigation to Itemized Product');
   };
   return (
     <div>
-      <center>
-        <button type="button" className="btn btn_asLink" onClick={handleClick}>
-          Products
-        </button>
-      </center>
+      <button type="button" className="btn btn_asLink" onClick={handleClick}>
+        Products
+      </button>
     </div>
   );
 }
