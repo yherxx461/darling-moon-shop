9e0e860bf5e586a1bf32a2340b410dc4073bc948
Ying Her <yher.prime@gmail.com>
2024-03-24 22:58:38 -0500

    removed ShoppingCartIcon out of AccountPage as planning to create it's own Page for it

--- src/components/AccountPage/AccountPage.jsx
+++ src/components/AccountPage/AccountPage.jsx
@@ -1,65 +1,58 @@
 import React from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import { useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
-import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
-import { useTheme } from '@mui/material/styles';
+// import { useHistory } from 'react-router-dom';
+
+// Material UI Imports
+// import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-
 import { Link } from '@mui/material';
+// import Badge from '@mui/material/Badge';
+
+// Import Component/Page:
+import ShoppingCartIconPage from '../ShoppingCart/ShoppingCartIconPage';
 
 function AccountPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
-  const history = useHistory();
+  // const history = useHistory();
 
-  const handleClick = () => {
-    history.push('/shoppingcart');
-    // console.log('navigate to Orders');
-  };
+  // const handleClick = () => {
+  //   history.push('/shoppingCart');
+  //   // console.log('navigate to Orders');
+  // };
   return (
     <div className="container">
-      <span className="span-container">
-        <Card sx={{ display: 'flex' }}>
-          <Box
-            sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'center',
-              // content: 'center,',
-            }}
-          >
-            <h2>Welcome, {user.username}!</h2>
-            <p>{user.name}</p>
-            <p>{user.email}</p>
-            <p>{user.phone}</p>
-            {/* <p>Your ID is: {user.id}</p> */}
-            {/* DO NOT SHOW USER ID. COMMENTED USER ID OUT. */}
-            {/* </Box>
-        </Card>
-        <Card sx={{ display: 'flex' }}>
-      <Box sx={{ display: 'flex', flexDirection: 'column' }}> */}{' '}
-            {/* <ShoppingCartOutlinedIcon /> */}
-            <LogOutButton className="btn" />
-          </Box>
-          <Box
-            sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'center',
-              // content: 'center',
-            }}
-          >
-            <Link to="/orders">
-              <ShoppingCartOutlinedIcon
-                className="shoppingCart"
-                onClick={handleClick}
-              />
-            </Link>
-          </Box>
-        </Card>
-      </span>
+      <Card sx={{ display: 'flex' }}>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            // content: 'center,',
+          }}
+        >
+          <h2>Welcome, {user.username}!</h2>
+          <p>{user.name}</p>
+          <p>{user.email}</p>
+          <p>{user.phone}</p>
+          {/* <p>Your ID is: {user.id}</p> */}
+          {/* DO NOT SHOW USER ID. COMMENTED USER ID OUT. */}{' '}
+          <LogOutButton className="btn" />
+        </Box>{' '}
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+          }}
+        >
+          <Link to="/shoppingCart">
+            <ShoppingCartIconPage />
+          </Link>
+        </Box>
+      </Card>
     </div>
   );
 }
