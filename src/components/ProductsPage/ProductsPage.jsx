bc9a40a178f57398b3d7348b34514f24064418f7
Ying Her <yher.prime@gmail.com>
2024-03-24 21:27:37 -0500

    added handleClick but not completed yet

--- src/components/ProductsPage/ProductsPage.jsx
+++ src/components/ProductsPage/ProductsPage.jsx
@@ -2,18 +2,14 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 
 function ProductsPage() {
-  const history = useHistory();
-
+  const handleClick = () => {
+    history.push('/products');
+    console.log('in handleClick navigation to Itemized Product');
+  };
   return (
     <div>
       <center>
-        <button
-          type="button"
-          className="btn btn_asLink"
-          onClick={() => {
-            history.push('/products');
-          }}
-        >
+        <button type="button" className="btn btn_asLink" onClick={handleClick}>
           Products
         </button>
       </center>
