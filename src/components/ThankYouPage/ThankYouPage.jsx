42830dc9ae46bf2bb7c633c981565e11385541a9
Ying Her <yher.prime@gmail.com>
2024-03-24 21:23:12 -0500

    handleClick function created but not compoleted yet

--- src/components/ThankYouPage/ThankYouPage.jsx
+++ src/components/ThankYouPage/ThankYouPage.jsx
@@ -1,17 +1,35 @@
-import { Button } from '@mui/material';
+import { Button, Link } from '@mui/material';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 
 function ThankYouPage() {
+  const handleClick = () => {
+    console.log('in handleClick');
+  };
+
   return (
     <div className="thankyou-messages">
       <p>Your order has been placed! Thank you for your business.</p>
-      <Button size="small" variant="outlined" startIcon={<ArrowBackIosIcon />}>
-        Back to Home
-      </Button>{' '}
-      <Button size="small" variant="outlined" endIcon={<ArrowForwardIosIcon />}>
-        Continue Shopping
-      </Button>
+      <Link to="/home">
+        <Button
+          onClick={handleClick}
+          size="small"
+          variant="outlined"
+          startIcon={<ArrowBackIosIcon />}
+        >
+          Back to Home
+        </Button>
+      </Link>{' '}
+      <Link to="/products">
+        <Button
+          onClick={handleClick}
+          size="small"
+          variant="outlined"
+          endIcon={<ArrowForwardIosIcon />}
+        >
+          Continue Shopping
+        </Button>
+      </Link>
     </div>
   );
 }
