e0a1f2d9f8a3c4be244dd81103c9aa2138038f69
Ying Her <yher.prime@gmail.com>
2024-03-28 17:40:49 -0500

    DELETE router worked in Postman

--- server/routes/address.router.js
+++ server/routes/address.router.js
@@ -26,6 +26,8 @@ router.post('/', (req, res) => {
   const zip = req.body.zip;
   const { user_id } = req.params;
 
+  /// {{{{ SHOULD USER_ID BE PART OF THE QUERY TEXT TOO OR SINCE IT'S ONLY ONE USER PER ACCOUNT, IT'S NOT NEEDED!?}}}}
+
   const queryText = `INSERT INTO "addresses" (street, city, state, zip, user_id)
     VALUES ($1, $2, $3, $4, $5) RETURNING id`;
   pool
@@ -33,6 +35,40 @@ router.post('/', (req, res) => {
     .then(() => res.sendStatus(201))
     .catch((err) => {
       console.log('Address not posting', err);
+      res.sendStatus(500);
+    });
+});
+
+// PUT route request -- {{{{{ THIS IS NOT WORKING YET!!!! }}}}}
+router.put('/:id', (req, res) => {
+  // POST route code
+  const { id } = req.params;
+  const queryText = `UPDATE "addresses" SET "street" = $1, city = $2, state = $3, zip = $4 WHERE "id" = $1`;
+  pool
+    .query(queryText, [id])
+    .then(() => {
+      console.log(`PUT /addresses/${id} - SUCCESS`);
+      res.sendStatus(201);
+    })
+    .catch((err) => {
+      console.log('Error: Internal error', err);
+      res.sendStatus(500);
+    });
+});
+module.exports = router;
+
+// PUT route request
+router.delete('/:id', (req, res) => {
+  // POST route code
+  const { id } = req.params;
+
+  const queryText = `DELETE FROM "addresses" WHERE "id" = $1;`;
+
+  pool
+    .query(queryText, [id])
+    .then(() => res.sendStatus(201))
+    .catch((err) => {
+      console.log('Unable to delete address', err);
       res.sendStatus(500);
     });
 });
