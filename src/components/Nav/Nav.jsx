46a9eada0194584540c6395670c7adeab1ac2ba6
Ying Her <yher.prime@gmail.com>
2024-03-27 19:59:36 -0500

    imported AccountAvatarIcon to Nav component

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,6 +3,9 @@ import { Link } from 'react-router-dom';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import './Nav.css';
 import { useSelector } from 'react-redux';
+// import AccountAvatarIcon from '../AccountAvatarIcon/AccountAvatarIcon';
+import AccountCircleOutlined from '@mui/icons-material/AccountCircleOutlined';
+import ShoppingCartIconPage from '../ShoppingCart/ShoppingCartIconPage';
 
 function Nav() {
   const user = useSelector((store) => store.user);
@@ -37,8 +40,12 @@ function Nav() {
         {/* If a user is logged in, show these links */}
         {user.id && (
           <>
+            <Link className="navLink" to="/cart">
+              <ShoppingCartIconPage />
+            </Link>
             <Link className="navLink" to="/account">
-              Account
+              {/* <AccountAvatarIcon /> */}
+              <AccountCircleOutlined fontSize="large" />
             </Link>
 
             {/* <Link className="navLink" to="/info">
