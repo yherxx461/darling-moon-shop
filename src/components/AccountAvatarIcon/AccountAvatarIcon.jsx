b2e1f8dda859256a77bf14cbcae6e9d0e2caa341
Ying Her <yher.prime@gmail.com>
2024-03-27 19:57:02 -0500

    created another component for the AvatarIcon so can imported to the Nav component

---
+++ src/components/AccountAvatarIcon/AccountAvatarIcon.jsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+
+// Material UI Imports
+import Avatar from '@mui/material/Avatar';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import { useSelector } from 'react-redux';
+
+function AccountAvatarIcon() {
+  const user = useSelector((store) => {
+    store.user;
+  });
+  return (
+    <>
+      <AccountCircleOutlinedIcon
+      // alt={user}
+      // src="/broken-image.jpg"
+      // sx={{ width: 42, height: 42 }}
+      />
+    </>
+  );
+}
+
+export default AccountAvatarIcon;
