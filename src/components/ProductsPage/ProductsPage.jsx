cdf45860ee01f0e70b68390526e4219943062808
Ying Her <yher.prime@gmail.com>
2024-03-31 23:28:08 -0500

    handleClickToItemizedProduct created and is clickbale

--- src/components/ProductsPage/ProductsPage.jsx
+++ src/components/ProductsPage/ProductsPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 // Material UI Imports
 import Grid from '@mui/material/Grid';
@@ -8,10 +9,13 @@ import Card from '@mui/material/Card';
 import Typography from '@mui/material/Typography';
 import CardContent from '@mui/material/CardContent';
 import Box from '@mui/material/Box';
+import { useParams } from 'react-router-dom';
 
 function ProductsPage() {
+  const id = useParams();
   const dispatch = useDispatch();
   const products = useSelector((store) => store.products);
+  const history = useHistory();
 
   useEffect(() => {
     // initial load  --> first load to load once
@@ -20,10 +24,13 @@ function ProductsPage() {
     });
   }, []);
 
+  const handleClickToItemizedProduct = () => {
+    history.push(`/products/${id}`);
+  };
+
   return (
     <div className="productImages">
       {/* <div className="productImages" key={products.id}> */}
-
       {/* TO-DO: Need to do fetching of all the products in the inventory */}
       {products.map((product) => {
         return (
@@ -38,7 +45,11 @@ function ProductsPage() {
                 variant="outlined"
                 className="container"
               >
-                <img src={product.image_1} />
+                <img
+                  src={product.image_1}
+                  className="productImage"
+                  onClick={handleClickToItemizedProduct}
+                />
                 <CardContent>
                   <Typography gutterBottom variant="p" component="div">
                     {product.name}
