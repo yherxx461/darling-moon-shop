92fe7e98caecfaafbd099f79c2e8224c024f5ead
Ying Her <yher.prime@gmail.com>
2024-04-02 13:30:02 -0500

    got ShoppingCart page to render the name and price

--- src/components/ShoppingCart/ShoppingCart.jsx
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -8,16 +8,17 @@ import { useEffect, useState } from 'react';
 
 function ShoppingCart({ item }) {
   // const [quantity, setQuantity] = useState('');
-  const [totalPrice, setTotalPrice] = useState(0);
-  const dispatch = useDispatch();
   const cart = useSelector((store) => store.cart);
+  const dispatch = useDispatch();
+  const [totalPrice, setTotalPrice] = useState(0);
 
-  useEffect(() => {
-    // fetch cart items
-    dispatch({
-      type: 'FETCH_CART',
-    });
-  }, [dispatch]);
+  // useEffect(() => {
+  //   // fetch cart items
+  //   dispatch({
+  //     type: 'FETCH_CART',
+  //   });
+  // }, [dispatch]);
+  // console.log('console.log', cart);
 
   useEffect(() => {
     // Total price whenever item in cart changes
@@ -28,84 +29,88 @@ function ShoppingCart({ item }) {
     setTotalPrice(total);
   }, [cart]);
 
-  const handleDeleteItem = (id) => {
-    console.log('in handleDeleteItem', id);
-  };
-
   const handleChange = (event, id) => {
     const newQuantity = parseInt(event.target.value);
-    // setQuantity(parseInt(event.target.value));
-    // console.log('update local state with quantity update');
-    //   dispatch({
-    //     type: 'UPDATE_QUANTITY_REQUEST',
-    //     payload: { id: item.id, quantity: newQuantity },
-    //   });
-    // };
+    dispatch({
+      type: 'UPDATE_QUANTITY_REQUEST',
+      payload: { id: id, quantity: newQuantity },
+    });
+  };
+  // setQuantity(parseInt(event.target.value));
+  // console.log('update local state with quantity update');
+  //   dispatch({
+  //     type: 'UPDATE_QUANTITY_REQUEST',
+  //     payload: { id: item.id, quantity: newQuantity },
+  //   });
+  // };
 
-    const handleOrderCheckout = (id) => {
-      console.log('in handleOrderCheckout');
-    };
-    return (
-      <>
-        <div>
-          <h3>Shopping Cart</h3>
-          {/* Mapping/fetching the item in the cart */}
-          {cart.map((item) => (
-            <Card key={item.id} className="selectedItems">
-              <img src={item.image_1} alt="item-selected" />
-              <p>{item.name}</p>
-              Qty:
-              <select
-                name="quantity"
-                value={quantity}
-                onChange={(event) => handleChange(event, item.id)}
-              >
-                {/* Limiting quantity to 10 only */}
-                {[...Array(10)].map((_, i) => (
-                  <option key={i + 1} value={i + 1}></option>
-                  //     {/* <option value="1">1</option>
-                  //   <option value="2">2</option>
-                  //   <option value="3">3</option>
-                  //   <option value="4">4</option>
-                  //   <option value="5">5</option>
-                  //   <option value="6">6</option>
-                  //   <option value="7">7</option>
-                  //   <option value="8">8</option>
-                  //   <option value="9">9</option>
-                  // <option value="10">10</option> */}
-                ))}
-              </select>
-              <p>Price: ${item.price} || 0</p>
-              <Button
-                size="small"
-                variant="outlined"
-                type="delete"
-                onClick={() => handleDeleteItem(item.id)}
-              >
-                Remove
-              </Button>
-            </Card>
-          ))}
-          <Card className="totalCost">
-            {/* TO-DO: Total */}
-            <p>
-              Total: ${' '}
-              {cart.reduct((total, item) => total + (item.price || 0), 0)}
-            </p>
-            {/* TO-DO: Order Checkout Button */}
+  const handleDeleteItem = (id) => {
+    dispatch({
+      type: 'DELETE_CART_ITEM',
+      payload: id,
+    });
+  };
+
+  const handleOrderCheckout = (id) => {
+    console.log('in handleOrderCheckout');
+  };
+  return (
+    <>
+      <div>
+        <h3>Shopping Cart</h3>
+        {/* Mapping/fetching the item in the cart */}
+        {cart.map((item) => (
+          <Card key={item.id} className="selectedItems">
+            <img src={item.image_1} alt="item-selected" />
+            <p>{item.name}</p>
+            Qty:
+            <select
+              name="quantity"
+              value={item.quantity}
+              onChange={(event) => handleChange(event, item.id)}
+            >
+              {/* Limiting quantity to 10 only */}
+              {[...Array(10)].map((_, i) => (
+                <option key={i + 1} value={i + 1}></option>
+                //     {/* <option value="1">1</option>
+                //   <option value="2">2</option>
+                //   <option value="3">3</option>
+                //   <option value="4">4</option>
+                //   <option value="5">5</option>
+                //   <option value="6">6</option>
+                //   <option value="7">7</option>
+                //   <option value="8">8</option>
+                //   <option value="9">9</option>
+                // <option value="10">10</option> */}
+              ))}
+            </select>
+            <p>Price: ${item.price} || 0</p>
             <Button
               size="small"
               variant="outlined"
-              type="submit"
-              onClick={handleOrderCheckout}
+              type="delete"
+              onClick={() => handleDeleteItem(item.id)}
             >
-              Order Checkout
+              Remove
             </Button>
           </Card>
-        </div>
-      </>
-    );
-  };
+        ))}
+        <Card className="totalCost">
+          {/* TO-DO: Total */}
+          <p>Total: ${totalPrice}</p>
+          {/* TO-DO: Order Checkout Button */}
+          <Button
+            size="small"
+            variant="outlined"
+            type="submit"
+            onClick={handleOrderCheckout}
+          >
+            Order Checkout
+          </Button>
+        </Card>
+      </div>
+    </>
+  );
 }
 
 export default ShoppingCart;
