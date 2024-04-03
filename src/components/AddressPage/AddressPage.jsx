0b71d9250657da1e80f1fa1b366ee08c6b59035a
Ying Her <yher.prime@gmail.com>
2024-04-03 16:54:41 -0500

    components updated

---
+++ src/components/AddressPage/AddressPage.jsx
@@ -0,0 +1,132 @@
+import { Button, Link } from '@mui/material';
+
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+
+function AddressesPage() {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const address = useSelector((store) => store.address);
+  const [newAddress, setNewAddress] = useState({
+    street: '',
+    city: '',
+    state: '',
+    zip: '',
+    isDefault: false,
+  });
+
+  useEffect(() => {
+    //initial load --> load once
+    dispatch({ type: 'FETCH_ADDRESS' });
+  }, []);
+
+  const handClickAddress = () => {
+    history.push('/address');
+  };
+
+  const handleClickAccount = () => {
+    history.push('/account');
+  };
+
+  const handleSubmitAddress = (event) => {
+    event.preventDefault();
+    dispatch({ type: 'ADD_ADDRESS', payload: newAddress });
+    setNewAddress({
+      street: '',
+      city: '',
+      state: '',
+      zip: '',
+      isDefault: false,
+    });
+    setNewAddress('');
+  };
+
+  const handleChange = (event) => {
+    const updatedAddress = event.target;
+    setNewAddress(updatedAddress);
+  };
+  return (
+    <>
+      <div>
+        <ul>
+          <li>
+            <Link
+              className="nav-link"
+              to="/account"
+              onClick={handleClickAccount}
+            >
+              Account
+            </Link>
+          </li>
+          <li>
+            <Link className="nav-link" to="/address" onClick={handClickAddress}>
+              Addresses
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <h3>Address</h3>
+      <form className="address-form" onSubmit={handleSubmitAddress}>
+        <br></br>
+        <input
+          type="text"
+          className="street"
+          placeholder="Street"
+          value={newAddress.street}
+          onChange={handleChange}
+        />
+        <br></br>
+        <input
+          type="text"
+          className="city"
+          placeholder="City"
+          value={newAddress.city}
+          onChange={handleChange}
+        />
+        <br></br>
+        <input
+          type="text"
+          className="state"
+          placeholder="State"
+          value={newAddress.state}
+          onChange={handleChange}
+        />
+        <br></br>
+        <input
+          type="text"
+          className="zip"
+          placeholder="Zip"
+          value={newAddress.zip}
+          onChange={handleChange}
+        />
+        <br></br>
+        <input
+          type="checkbox"
+          className="set-default"
+          checked={newAddress.isDefault}
+          onChange={handleChange}
+        />
+        Set as default address
+        <br></br>
+        <Button variant="outlined" size="small">
+          Confirm
+        </Button>
+      </form>
+      <>
+        <h3>Saved Addresses</h3>
+        {address.map((addressItem) => (
+          <div key={addressItem.id}>
+            <p>{addressItem.street}</p>
+            <p>
+              {addressItem.city}, {addressItem.state} {addressItem.zip}
+            </p>
+          </div>
+        ))}
+        ;
+      </>
+    </>
+  );
+}
+
+export default AddressesPage;
