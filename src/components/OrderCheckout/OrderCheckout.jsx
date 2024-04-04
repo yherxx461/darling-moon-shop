47cac3db4dba080e3856023d57365e78c962dd8b
Ying Her <yher.prime@gmail.com>
2024-04-04 08:26:59 -0500

    orderCheckout page created and mapped all products --> need to modified so that it only shows the items that are in the cart only

--- src/components/OrderCheckout/OrderCheckout.jsx
+++ src/components/OrderCheckout/OrderCheckout.jsx
@@ -1,13 +1,25 @@
-import { Box } from '@mui/material';
+import { Box, Link } from '@mui/material';
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
-
+import { useHistory } from 'react-router-dom';
+// import './OrderCheckout.css';
 // imports
 
 function OrderCheckout() {
   const user = useSelector((store) => store.user);
   const address = useSelector((store) => store.address);
+  const cart = useSelector((store) => store.cart);
   const dispatch = useDispatch();
+  const history = useHistory();
+
+  useEffect(() => {
+    // Dispatch an action to fetch products when the component mounts
+    dispatch({ type: 'FETCH_CART' });
+  }, [dispatch]);
+
+  const handleSubmitOrderCheckout = () => {
+    history.push('/order-confirmation');
+  };
 
   return (
     <>
@@ -15,13 +27,33 @@ function OrderCheckout() {
         <h1>Order Checkout</h1>
       </div>
       <div>
-        <p>Ship to Address:</p>
+        <button onClick={handleSubmitOrderCheckout}>
+          <Link to="/order-confirmation">Order Checkout</Link>
+        </button>
+      </div>
+      <div>
+        <p className="page-title">Ship To:</p>
       </div>
       <div className="shipping-address">
         <Box>
           <p>{user.name}</p>
-          <p>1234 Main Street</p>
+          <p>{address.street}1234 Main Street</p>
           <p>New York, New York 10044</p>
+        </Box>
+      </div>
+      <div>
+        <Box className="item-list">
+          <p>Orders</p>
+          {cart.map((product) => (
+            <div key={product.id}>
+              <img src={product.image_1} />
+              <p>Product Name</p>
+              <p>
+                $ {product.price.toFixed(2)} x {product.quantity}
+              </p>
+              <p></p>
+            </div>
+          ))}
         </Box>
       </div>
     </>
