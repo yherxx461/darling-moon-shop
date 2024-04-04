610af1c0750491761c2930016b1d1bcfd51753e8
Ying Her <yher.prime@gmail.com>
2024-04-04 08:29:41 -0500

    combined all delete, fetch, add, update saga to address

--- src/redux/sagas/address.saga.js
+++ src/redux/sagas/address.saga.js
@@ -11,18 +11,41 @@ function* fetchAddress() {
   }
 }
 
-function* addAddressSaga(action) {
+function* addAddress(action) {
   try {
     yield axios.post(`/api/address/${action.payload}`, action.payload);
-    yield put({ type: 'FETCH_ADDRESS' }); // fetch address again after adding a new address
+    yield put({ type: 'ADD_ADDRESS' }); // fetch address again after adding a new address
+    yield put({ type: 'FETCH_ADDRESS' });
   } catch (error) {
     console.log('Error adding new address', error);
   }
 }
 
+function* deleteAddress(action) {
+  try {
+    yield axios.delete(`/api/address/${action.payload}`, action.payload);
+    yield put({ type: 'DELETE_ADDRESS' });
+    yield put({ type: 'FETCH_ADDRESS' });
+  } catch (error) {
+    console.log('Error in deleting address', error);
+  }
+}
+
+function* updateAddress(action) {
+  try {
+    yield axios.put(`/api/address/${action.payload}`, action.payload);
+    yield put({ type: 'UPDATE_ADDRESS' });
+    yield put({ type: 'FETCH_ADDRESS' });
+  } catch (error) {
+    console.log('Error in deleting address', error);
+  }
+}
+
 function* addressSaga() {
   yield takeLatest('FETCH_ADDRESS', fetchAddress);
-  yield takeLatest('ADD_ADDRESS', addAddressSaga);
+  yield takeLatest('ADD_ADDRESS', addAddress);
+  yield takeLatest('DELETE_ADDRESS', deleteAddress);
+  yield takeLatest('UPDATE_ADDRESS', updateAddress);
 }
 
 export default addressSaga;
