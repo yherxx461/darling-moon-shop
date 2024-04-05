e151e2d2deaec8f476f25d33bf3d3485f8306abc
Ying Her <yher.prime@gmail.com>
2024-04-04 19:39:22 -0500

    modified get request to dispatch image

--- src/redux/sagas/cart.saga.js
+++ src/redux/sagas/cart.saga.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { put, takeLatest } from 'redux-saga/effects';
 
 // worker Saga: will be fired on "FETCH_CART" actions
-function* fetchCartItems() {
+function* fetchCart() {
   try {
     const response = yield axios.get('/api/cart');
     const cartItems = response.data.map((item) => ({
@@ -14,9 +14,25 @@ function* fetchCartItems() {
     console.log('Cart get request failed', error);
   }
 }
+function* addToCart(action) {
+  try {
+    // Send Post request to add item to the cart
+    yield axios.post('/api/cart', action.payload);
+    // After adding item to cart, fetch all items added to the cart
+    yield put({ type: 'FETCH_CART' });
+    // Dispatch SET_CART action to update cart items after adding them to the cart
+
+    if (Array.isArray(action.payload)) {
+      yield put({ type: 'SET_CART', payload: action.payload });
+    }
+  } catch (error) {
+    console.log('Error in updating new item to cart', error);
+  }
+}
 
 function* cartSaga() {
-  yield takeLatest('FETCH_CART', fetchCartItems);
+  yield takeLatest('FETCH_CART', fetchCart);
+  yield takeLatest('ADD_TO_CART', addToCart);
 }
 
 export default cartSaga;
