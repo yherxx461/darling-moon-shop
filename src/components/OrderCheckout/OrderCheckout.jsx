f9a8d8a8e742da8e00b7ec59e5e4ed57b1223b03
Ying Her <yher.prime@gmail.com>
2024-04-16 08:43:23 -0500

    orderCheckout page styled as table

--- src/components/OrderCheckout/OrderCheckout.jsx
+++ src/components/OrderCheckout/OrderCheckout.jsx
@@ -1,5 +1,5 @@
 // import {Snackbar, Alert } from '@mui/material';
-import { Link, Button, TextField } from '@mui/material';
+import { Link, Button } from '@mui/material';
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
@@ -25,8 +25,6 @@ function OrderCheckout() {
 
   const [totalPrice, setTotalPrice] = useState(0);
   const [defaultAddress, setDefaultAddress] = useState(null);
-  const [editMode, setEditMode] = useState(false);
-  const [editedAddress, setEditedAddress] = useState({});
 
   useEffect(() => {
     // Dispatch an action to fetch products when the component mounts
@@ -52,21 +50,6 @@ function OrderCheckout() {
     const defaultAddress = addresses.find((address) => address.is_default);
     setDefaultAddress(defaultAddress);
   }, [addresses]);
-
-  const handleEditAddress = () => {
-    setEditedAddress(defaultAddress);
-    setEditMode(true);
-  };
-  const handleSaveAddress = () => {
-    // dispatch action to update address in redux
-    dispatch({ type: 'UPDATE_ADDRESS', payload: editedAddress });
-    setEditMode(false);
-  };
-
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setEditedAddress({ ...editedAddress, [name]: value });
-  };
 
   const handlePlaceOrder = () => {
     if (!defaultAddress) {
@@ -110,67 +93,19 @@ function OrderCheckout() {
       </div>
       <h2 className="ship-to-title">Ship To:</h2>
       <div className="address-container">
-        {defaultAddress && ! editMode && (
-          // Render input fields for editing
-          <div className="shipping-address">
-            <TextField
-              name="name"
-              label="Name"
-              size="small"
-              className="edit-address-field"
-              value={editedAddress.name}
-              onChange={handleInputChange}
-            />
-            <br></br>
-            <TextField
-              name="street"
-              label="Street"
-              size="small"
-              className="edit-address-field"
-              value={editedAddress.street}
-              onChange={handleInputChange}
-            />
-            <br></br>
-
-            <TextField
-              name="city"
-              label="City"
-              size="small"
-              className="edit-address-field"
-              value={editedAddress.city}
-              onChange={handleInputChange}
-            />
-            <br></br>
-
-            <TextField
-              name="zip"
-              label="Zip"
-              size="small"
-              className="edit-address-field"
-              value={editedAddress.zip}
-              onChange={handleInputChange}
-            />
-            <br></br>
-
-            {/* Add other address fields as needed */}
-            <Button variant="contained" onClick={handleSaveAddress}>
-              Save
-            </Button>
-          </div>
-        ) : defaultAddress ? (
-          // render only the default address
+        {defaultAddress ? (
+          // render only the default address is not null
           <div key={defaultAddress.id} className="shipping-address">
             <div>
-              <p>{user.name}</p>
-              <p>{defaultAddress.street}</p>
-              <p>
+              <p style={{ justifyContent: 'flex-start' }}>{user.name}</p>
+              <p style={{ justifyContent: 'flex-start' }}>
+                {defaultAddress.street}
+              </p>
+              <p style={{ justifyContent: 'flex-start' }}>
                 {defaultAddress.city}, {defaultAddress.state}{' '}
                 {defaultAddress.zip}
               </p>
             </div>
-            <Button variant="outlined" size="small" onClick={handleEditAddress}>
-              Edit
-            </Button>
           </div>
         ) : (
           <p>No address found</p>
@@ -178,12 +113,7 @@ function OrderCheckout() {
       </div>
       <h2 className="orders">Orders</h2>
       {cart.map((item) => (
-        <TableContainer
-          key={item.id}
-          className="order-container"
-          component={Paper}
-          sx={{ maxWidth: 1000 }}
-        >
+        <TableContainer key={item.id} component={Paper} align="center">
           <Table sx={{ maxWidth: 1000 }} aria-label="simple table">
             <TableHead>
               <TableRow>
@@ -218,7 +148,7 @@ function OrderCheckout() {
                   {item.product_name}
                 </TableCell>
                 <TableCell align="center" style={{ width: '33%' }}>
-                  {item.quantity} qty
+                  {item.quantity}
                 </TableCell>
               </TableRow>
             </TableBody>
